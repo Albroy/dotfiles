4efbaca647b46a54e7e4620311fdd60c7b96d5de
Albroy <94757950+Albroy@users.noreply.github.com>
2026-07-02 15:54:22 +0200

    add md support

---
+++ editors/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,10 @@
+---@type LazySpec
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+  ft = { "markdown" },
+  opts = {},
+}
